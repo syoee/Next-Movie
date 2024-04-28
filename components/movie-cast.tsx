9e90511793c5eeae1328aa7faa7b2b766f3ca0f7
syoee <tjsdyd1347@gmail.com>
2024-04-28 21:00:01 +0900

    Modify: slick css

--- components/movie-cast.tsx
+++ components/movie-cast.tsx
@@ -1,17 +1,64 @@
+"use client";
+
 import { API_URL } from "../app/constants";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import human from "../styles/movie-cast.module.css";
 
 async function getCredits(id: string) {
 	const response = await fetch(`${API_URL}/${id}/credits`);
 	return response.json();
 }
 
+var settings = {
+	dots: false,
+	infinite: false,
+	speed: 500,
+	slidesToShow: 6,
+	slidesToScroll: 3,
+	initialSlide: 0,
+	responsive: [
+		{
+			breakpoint: 1024,
+			settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: false,
+				dots: false,
+			},
+		},
+		{
+			breakpoint: 600,
+			settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2,
+				initialSlide: 0,
+			},
+		},
+		{
+			breakpoint: 480,
+			settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1,
+				initialSlide: 0,
+			},
+		},
+	],
+};
+
 const MovieCast = async ({ id }: { id: string }) => {
 	const credits = await getCredits(id);
 	return (
-		<div>
-			{credits.map((credits) => (
-				<img src={credits.profile_path} alt={credits.original_name} />
-			))}
+		<div className={human.container}>
+			<Slider {...settings}>
+				{credits.map((credits) => (
+					<div className={human.content}>
+						<img src={credits.profile_path} alt={credits.original_name} />
+						<div>{credits.name}</div>
+					</div>
+				))}
+			</Slider>
 		</div>
 	);
 };
