34e99880f47036236ca5c4e713258b228c0d6d90
syoee <tjsdyd1347@gmail.com>
2024-05-07 16:53:34 +0900

    ADD: info-navigation

---
+++ components/info-navigation.tsx
@@ -0,0 +1,30 @@
+import { API_URL } from "../app/constants";
+import potato from "../styles/movie-info.module.css";
+
+export async function getMovie(id: string) {
+	const response = await fetch(`${API_URL}/${id}`);
+	return response.json();
+}
+
+const MovieInfo = async ({ id }: { id: string }) => {
+	const movie = await getMovie(id);
+	return (
+		<div className={potato.container}>
+			<img
+				src={movie.poster_path}
+				className={potato.poster}
+				alt={movie.title}
+			/>
+			<div className={potato.info}>
+				<h1 className={potato.title}>{movie.title}</h1>
+				<h3>⭐️{movie.vote_average.toFixed(1)}</h3>
+				<p>{movie.overview}</p>
+				<a href={movie.homepage} target={"_blank"}>
+					&rarr; HomePage
+				</a>
+			</div>
+		</div>
+	);
+};
+
+export default MovieInfo;
