c3612934d0db2fb1000b188d010787003cb17e07
syoee <tjsdyd1347@gmail.com>
2024-05-08 22:52:58 +0900

    Modify: info-nav css

--- components/info-navigation.tsx
+++ components/info-navigation.tsx
@@ -1,5 +1,4 @@
 import { API_URL } from "../app/constants";
-import potato from "../styles/movie-info.module.css";
 
 export async function getMovie(id: string) {
 	const response = await fetch(`${API_URL}/${id}`);
@@ -9,14 +8,10 @@ export async function getMovie(id: string) {
 const MovieInfo = async ({ id }: { id: string }) => {
 	const movie = await getMovie(id);
 	return (
-		<div className={potato.container}>
-			<img
-				src={movie.poster_path}
-				className={potato.poster}
-				alt={movie.title}
-			/>
-			<div className={potato.info}>
-				<h1 className={potato.title}>{movie.title}</h1>
+		<div>
+			<img src={movie.poster_path} alt={movie.title} />
+			<div>
+				<h1>{movie.title}</h1>
 				<h3>⭐️{movie.vote_average.toFixed(1)}</h3>
 				<p>{movie.overview}</p>
 				<a href={movie.homepage} target={"_blank"}>
